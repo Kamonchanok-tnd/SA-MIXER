4c508cc05b3ed80994a6386894c83318857efbcd
Warawut <pleumkj@gmail.com>
2024-09-17 20:57:13 +0700

    sa15000

--- backend/config/db.go
+++ backend/config/db.go
@@ -79,7 +79,7 @@ func SetupDatabase() {
 		Email:      "sa2@gmail.com",
 		Password:   hashedPassword2,
 		GenderID:   GenderMale.ID,
-		TotalPoint: 1500,
+		TotalPoint: 150,
 		Role:       "customer",
 	}
 	db.FirstOrCreate(Member2, &entity.Member{
